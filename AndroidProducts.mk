472703046f2f27ccf351d4b71025f42b9b32620f
vytska69 <vytautas.duchas@gmail.com>
2021-12-08 14:10:47 +0200

    cherish for olives is coming

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,4 @@
-#
+    #
 # Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_olives.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_olives-user \
-    lineage_olives-userdebug \
-    lineage_olives-eng
+    cherish_olives-user \
+    cherish_olives-userdebug \
+    cherish_olives-eng
